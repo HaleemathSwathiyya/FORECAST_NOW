94347f2b2962b61e1f00c1a251b682273e1e78d3
HaleemathSwathiyya <Haleemath.Swathiyya@ust.com>
2023-06-20 16:02:29 +0530

    changes

--- forecastnow/src/app/login/login.component.ts
+++ forecastnow/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')]),
+      password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')])
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Perform login logic
+      console.log('Form submitted');
+    } else {
+      // Handle invalid form
+      console.log('Invalid form');
+    }
+  }
 
 }
--- forecastnow/src/app/register/register.component.ts
+++ forecastnow/src/app/register/register.component.ts
@@ -10,6 +10,7 @@
 // }
 
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -17,9 +18,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  registrationForm: FormGroup;
+
+  constructor() {
+    this.registrationForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.pattern('[A-Za-z\s]+')]),
+      email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')]),
+      password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')])
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      // Perform registration logic
+      console.log('Form submitted');
+    } else {
+      // Handle invalid form
+      console.log('Invalid form');
+    }
+  }
+
+
   // register() {
   //   // Here you can add code to handle the registration logic
   //   // For example, send the registration data to a server
   //   // or perform any other necessary actions
   // }
+
+  
 }
